10cd934d97cc3518f7270fdb7e8ed7a7ea6d0a30
Michael Gribben <michaelgribbeninfo@gmail.com>
2020-05-17 09:13:13 +1000

    updated link and removed console.logs

--- site/assets/js/fun.js
+++ site/assets/js/fun.js
@@ -7,8 +7,6 @@
   );
 
   document.body.addEventListener("mousemove", evt => {
-    // var elem = document.querySelector( ".regions g:hover .mouseArea");
-    // if (elem) console.log(elem.getAttribute('data-text'))
     x.innerText = evt.clientX;
     y.innerText = evt.clientY;
   });
